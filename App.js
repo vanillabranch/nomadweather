79468b3bdf49fe80bf6a06b894973c735d43f607
vanillabranch <lockdown@kakao.com>
2021-11-14 00:53:27 +0900

    nomad-weather app initial

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello! I made a RN App!!</Text>
       <StatusBar style="auto" />
     </View>
   );
