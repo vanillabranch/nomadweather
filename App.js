972489486188983f97b890353c29c54d235fb23d
vanillabranch <lockdown@kakao.com>
2021-11-15 01:21:41 +0900

    스크롤뷰의 활용, 스크롤뷰의 수많은 prop들을 이용하여, 페이징, 등등

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
 
+const {width: SCREEN_WIDTH} = Dimensions.get("window");
+
+//console.log(SCREEN_WIDTH);
 
 export default function App() {
     return (
@@ -8,12 +11,33 @@ export default function App() {
             <View style={styles.city}>
                 <Text style={styles.cityName}>Seoul</Text>
             </View>
-            <View style={styles.weather}>
+            <ScrollView
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                indicatorStyle="white" /*  ios에서만 작동함. */
+                contentContainerStyle={styles.weather}>
+                {/* 일자별 View컴포넌트  1*/}
                 <View style={styles.day}>
                     <Text style={styles.temp}>27</Text>
                     <Text style={styles.description}>Sunny</Text>
                 </View>
-            </View>
+                {/* 일자별 View컴포넌트  2*/}
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                {/* 일자별 View컴포넌트  3*/}
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                {/* 일자별 View컴포넌트  4*/}
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -25,26 +49,26 @@ const styles = StyleSheet.create({
     },
     city: {
         flex: 1,
-        justifyContent:"center",
-        alignItems:"center"
+        justifyContent: "center",
+        alignItems: "center"
     },
-    cityName:{
-        fontSize:68,
-        fontWeight:"500"
+    cityName: {
+        fontSize: 68,
+        fontWeight: "500"
     },
-    weather:{
-        flex:3
+    weather: {
+
     },
-    day:{
-        flex:1,
-        alignItems:"center"
+    day: {
+        width: SCREEN_WIDTH,
+        alignItems: "center",
     },
-    temp:{
-        marginTop:50,
-        fontSize:178
+    temp: {
+        marginTop: 50,
+        fontSize: 178
     },
-    description:{
-        marginTop:-30,
-        fontSize:60
+    description: {
+        marginTop: -30,
+        fontSize: 60
     }
 });
