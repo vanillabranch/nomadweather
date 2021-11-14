ce936411db140eb8198da3880d6e6b0342b092bf
vanillabranch <lockdown@kakao.com>
2021-11-15 00:21:48 +0900

    RN의 flex활용방법과 웹브라우저와의 차이, 기본값 column, 웹은 row 등등

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View} from 'react-native';
 
-/*일부컴포넌트 들을 expo에서 import 해오는 이유*/
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={{flex : 1}}>
+            <View style={{flex:1, backgroundColor: "tomato"}}></View>
+            <View style={{flex:1, backgroundColor: "teal"}}></View>
+            <View style={{flex:1, backgroundColor: "orange"}}></View>
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
-  },
-   text:{
-       fontSize:28,
-       color:"black"
-   },
-});
